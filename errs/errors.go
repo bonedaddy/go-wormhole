9d29c577d7ef8dc6893daae1cd0fba5c89cc09af
Chris Pikul <ChrisPikul510@gmail.com>
2019-07-26 09:40:44 +0300

    switched to custom error for client ones

--- errs/errors.go
+++ errs/errors.go
@@ -2,21 +2,46 @@ package errs
 
 import "errors"
 
-//Common error types, these are copied
-//from the python version directly
+//ClientError shadows a normal error
+//so we can check against internal errors vs.
+//client errors. Client errors get written
+//direct to the client as is
+type ClientError struct {
+	message string
+}
+func (e ClientError) Error() string {
+	return e.message
+}
+//New creates and returns a new ClientError
+//with the provided message
+func New(msg string) ClientError {
+	return ClientError{ msg }
+}
+
+//ErrInternal is delivered to the client instead of
+//whatever server generated error would be,
+//this is to purposely be vague to the client since
+//it isn't their problem anyways
+var ErrInternal = errors.New("internal server error")
+
+//Common error types, most of these are copied
+//from the python version directly in case
+//clients reference them directly
 var (
-	ErrInternal = errors.New("internal server error")
+	ErrMissingType = New("missing 'type'")
+	ErrUnknownType = New("unknown type")
+	ErrPing        = New("ping requires 'ping'")
 
-	ErrMissingType = errors.New("missing 'type'")
-	ErrUnknownType = errors.New("unknown type")
-	ErrPing        = errors.New("ping requires 'ping'")
+	ErrBindFirst = New("must bind first")
+	ErrBound     = New("already bound")
+	ErrBindAppID = New("bind requires 'appid'")
+	ErrBindSide  = New("bind requires 'side'")
 
-	ErrBindFirst = errors.New("must bind first")
-	ErrBound     = errors.New("already bound")
-	ErrBindAppID = errors.New("bind requires 'appid'")
-	ErrBindSide  = errors.New("bind requires 'side'")
-
-	ErrAlreadyAllocated = errors.New("you already allocated one, don't be greedy")
-	ErrNameplateCrowded = errors.New("nameplate crowded")
-	ErrMailboxCrowded = errors.New("mailbox crowded")
+	ErrAlreadyAllocated = New("you already allocated one, don't be greedy")
+	ErrAlreadyClaimed = New("only one claim per connection")
+	ErrClaimNameplate = New("claim requires 'nameplate'")
+	
+	ErrNameplateCrowded = New("nameplate crowded")
+	ErrReclaimNameplate = New("you cannot re-claim a nameplate that your side previously released")
+	ErrMailboxCrowded = New("mailbox crowded")
 )
